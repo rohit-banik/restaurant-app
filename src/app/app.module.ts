698b554a5e562f9714e7eebf6224fc151bd185f6
Ruturaj Sanjay Kalshetti <jkalshr@jci.com>
2023-04-19 10:14:54 +0530

    Frontend

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductCardComponent } from './components/card/productcard.component';
+import { DishesComponent } from './views/dishes/dishes.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AlertComponent } from './components/alert/alert.component';
+import { PromptComponent } from './components/prompt/prompt.component';
+import { CreateComponent } from './views/create/create.component';
+import { EditComponent } from './views/edit/edit.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductCardComponent,
+    DishesComponent,
+    NavbarComponent,
+    AlertComponent,
+    PromptComponent,
+    CreateComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
