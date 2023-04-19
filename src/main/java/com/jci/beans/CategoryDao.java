d34c33413c67c460955162e468efcd59a59b9752
Rohit Banik <jbanikr@jci.com>
2023-04-19 16:17:08 +0530

    dish controllers updated

--- src/main/java/com/jci/beans/CategoryDao.java
+++ src/main/java/com/jci/beans/CategoryDao.java
@@ -2,36 +2,46 @@ package com.jci.beans;
 
 import com.jci.models.Category;
 import com.jci.models.Dish;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Repository
 public class CategoryDao {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
     List<Category> categories = new ArrayList<Category>(
-            Arrays.asList(
-                    new Category(1, "Veg", "Vegetables only", "https://uploads-ssl.webflow.com/5c481361c604e53624138c2f/5dcbca1717dc0960f009aadb_biryani.jpg"),
-                    new Category(2, "Nonveg", "Chicken and Fish only", "https://www.ticklingpalates.com/wp-content/uploads/2020/05/fried-rice-using-veggies.jpg"),
-                    new Category(3, "Starter", "Start like a king", "https://static.toiimg.com/thumb/75581339.cms?imgsize=1753029&width=800&height=800"),
-                    new Category(4, "Dessert", "A Sweet Ending", "https://images.immediate.co.uk/production/volatile/sites/30/2018/06/Vegan-salad-bowl-499145d.jpg?quality=90&resize=556,505")
-            )
+//            Arrays.asList(
+//                    new Category(1, "Veg", "Vegetables only", "https://uploads-ssl.webflow.com/5c481361c604e53624138c2f/5dcbca1717dc0960f009aadb_biryani.jpg"),
+//                    new Category(2, "Nonveg", "Chicken and Fish only", "https://www.ticklingpalates.com/wp-content/uploads/2020/05/fried-rice-using-veggies.jpg"),
+//                    new Category(3, "Starter", "Start like a king", "https://static.toiimg.com/thumb/75581339.cms?imgsize=1753029&width=800&height=800"),
+//                    new Category(4, "Dessert", "A Sweet Ending", "https://images.immediate.co.uk/production/volatile/sites/30/2018/06/Vegan-salad-bowl-499145d.jpg?quality=90&resize=556,505")
+//            )
     );
 
     public CategoryDao() {
+        String createTableSql = "CREATE TABLE category(categoryId int primary key, categoryName varchar(255), categoryDesc varchar(1000), categoryImage varchar(255), menuId int)";
     }
 
-    public List<Category> getAllCategory(){ return categories; }
+//    public List<Category> getAllCategory(){ return categories; }
 
     public Category getCategoryById(@PathVariable int id) {
-        Category fetchedCategory = null;
-        for(Category category:categories) {
-            if (category.getCategoryID() == id) {
-                fetchedCategory = category;
-                break;
-            }
-        }
-        return fetchedCategory;
+        String selectSqlbyId = "select * from category where dishId = ? and isDeleted = 0";
+        return jdbcTemplate.queryForObject(selectSqlbyId, new Object[]{id}, new BeanPropertyRowMapper<Category>(Category.class));
+//        Category fetchedCategory = null;
+//        for(Category category:categories) {
+//            if (category.getCategoryID() == id) {
+//                fetchedCategory = category;
+//                break;
+//            }
+//        }
+//        return fetchedCategory;
     }
 
     public Category addCategory(Category category) {
--- src/main/java/com/jci/beans/DishDao.java
+++ src/main/java/com/jci/beans/DishDao.java
@@ -4,11 +4,13 @@ import com.jci.models.Dish;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 @Repository
@@ -28,59 +30,67 @@ public class DishDao {
     public DishDao() {
     }
 
+
     public List<Dish> getAllDishes() {
-        String sql = "select * from dish";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Dish>(Dish.class));
+        String selectSql = "select * from dish where isDeleted = 0";
+        return jdbcTemplate.query(selectSql, new BeanPropertyRowMapper<Dish>(Dish.class));
+//      return dishes;
     }
-//    public List<Dish> getAllDishes(){
-//        return dishes;
-//    }
 
     public Dish getDishById(@PathVariable int id) {
-        Dish fetchedDish = null;
-        for(Dish dish:dishes) {
-            if (dish.getDishId() == id) {
-                fetchedDish = dish;
-                break;
-            }
-        }
-        return fetchedDish;
+        String selectSqlbyId = "select * from dish where dishId = ? and isDeleted = 0";
+        return jdbcTemplate.queryForObject(selectSqlbyId, new Object[]{id}, new BeanPropertyRowMapper<Dish>(Dish.class));
+//        Dish fetchedDish = null;
+//        for(Dish dish:dishes) {
+//            if (dish.getDishId() == id) {
+//                fetchedDish = dish;
+//                break;
+//            }
+//        }
+//        return fetchedDish;
     }
 
-    public Dish getDishByCategory(@PathVariable int id) {
-        ArrayList<Integer> categoryDish = new ArrayList<Integer>();
-        for(Dish dish:dishes) {
-            if (dish.getCategoryId() == id) {
-                categoryDish.add(id);
-            }
-        }
-        return (Dish) dishes;
-    }
+//    public Dish getDishByCategory(@PathVariable int id) {
+//        ArrayList<Integer> categoryDish = new ArrayList<Integer>();
+//        for(Dish dish:dishes) {
+//            if (dish.getCategoryId() == id) {
+//                categoryDish.add(id);
+//            }
+//        }
+//        return (Dish) dishes;
+//    }
 
-    public Dish addDish(Dish dish) {
-        dishes.add(dish);
-        return dish;
+    public void addDish(Dish dish) {
+        String insertSql = "insert into dish (dishId, dishName, dishDesc, dishPrice, dishImage, dishNature, categoryId) values(?, ?, ? , ?, ?, ?, ?)";
+        jdbcTemplate.update(insertSql, dish.getDishId(), dish.getDishName(), dish.getDishDesc(), dish.getDishPrice(), dish.getDishImage(), dish.getDishNature(), dish.getCategoryId());
+        System.out.println("Dish inserted");;
+//        dishes.add(dish);
+//        return dish;
     }
 
     public void deleteDishById(int id) {
-        Iterator<Dish> it = dishes.iterator();
-        while(it.hasNext()) {
-            if(it.next().getDishId() == id) {
-                it.remove();
-                System.out.println("Dish Deleted");
-            }
-        }
+        String softDeleteSql = "update dish set isDeleted = 1 where dishId = ?";
+        jdbcTemplate.update(softDeleteSql, id);
+//        Iterator<Dish> it = dishes.iterator();
+//        while(it.hasNext()) {
+//            if(it.next().getDishId() == id) {
+//                it.remove();
+//                System.out.println("Dish Deleted");
+//            }
+//        }
     }
 
     public void updateDishById(int id, Dish dish) {
-        for (Dish dishes : dishes) {
-            if (dishes.getDishId() == id) {
-                dishes.setDishName(dish.getDishName());
-                dishes.setDishDesc(dish.getDishDesc());
-                dishes.setDishPrice(dish.getDishPrice());
-                dishes.setDishImage(dish.getDishImage());
-                dishes.setDishNature(dish.getDishNature());
-            }
-        }
+        String softDeleteSql = "update dish set dishName = ?, dishDesc = ?, dishPrice = ?, dishImage = ?, dishNature = ?, categoryId = ? where dishId = ?";
+        jdbcTemplate.update(softDeleteSql, dish.getDishName(), dish.getDishDesc(), dish.getDishPrice(), dish.getDishNature(), dish.getCategoryId());
+//        for (Dish dishes : dishes) {
+//            if (dishes.getDishId() == id) {
+//                dishes.setDishName(dish.getDishName());
+//                dishes.setDishDesc(dish.getDishDesc());
+//                dishes.setDishPrice(dish.getDishPrice());
+//                dishes.setDishImage(dish.getDishImage());
+//                dishes.setDishNature(dish.getDishNature());
+//            }
+//        }
     }
 }
